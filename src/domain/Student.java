c79a3685aef856a4fb0692bac7bc3134b3450d85
parsa <hoseininejad1999@gmail.com>
2021-06-21 19:27:27 +0430

    Reformat Student file

--- src/domain/Student.java
+++ src/domain/Student.java
@@ -1,34 +1,35 @@
 package domain;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class Student {
-	private final String id;
-	private final String name;
-	private final Map<Term, Map<Course, Double>> transcript;
-	private final List<OfferedCourse> currentSemesterCourses;
+    private final String id;
+    private final String name;
+    private final Map<Term, Map<Course, Double>> transcript;
+    private final List<OfferedCourse> currentSemesterCourses;
 
-	public Student(String id, String name) {
-		this.id = id;
-		this.name = name;
-		this.transcript = new HashMap<>();
-		this.currentSemesterCourses = new ArrayList<>();
-	}
-	
-	public void takeCourse(Course course, int section) {
-		currentSemesterCourses.add(new OfferedCourse(course, section));
-	}
+    public Student(String id, String name) {
+        this.id = id;
+        this.name = name;
+        this.transcript = new HashMap<>();
+        this.currentSemesterCourses = new ArrayList<>();
+    }
 
-	public Map<Term, Map<Course, Double>> getTranscript() {
-		return transcript;
-	}
+    public void takeCourse(Course course, int section) {
+        currentSemesterCourses.add(new OfferedCourse(course, section));
+    }
 
-	public void addTranscriptRecord(Course course, Term term, double grade) {
-	    if (!transcript.containsKey(term))
-	        transcript.put(term, new HashMap<>());
-	    transcript.get(term).put(course, grade);
+    public void addTranscriptRecord(Course course, Term term, double grade) {
+        if (!transcript.containsKey(term))
+            transcript.put(term, new HashMap<>());
+        transcript.get(term).put(course, grade);
+    }
+
+    public Map<Term, Map<Course, Double>> getTranscript() {
+        return transcript;
     }
 
     public List<OfferedCourse> getCurrentSemesterCourses() {
@@ -36,15 +37,15 @@ public class Student {
     }
 
     public String getId() {
-		return id;
-	}
+        return id;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	@Override
-	public String toString() {
-		return name;
-	}
+    @Override
+    public String toString() {
+        return name;
+    }
 }
