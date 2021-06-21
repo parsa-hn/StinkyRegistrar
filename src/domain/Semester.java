52c6d87735b89d1b68161158b838b25f587af4e4
parsa <hoseininejad1999@gmail.com>
2021-06-21 19:38:38 +0430

    Add override annotation and reformat Semester

--- src/domain/Semester.java
+++ src/domain/Semester.java
@@ -1,35 +1,31 @@
 package domain;
 
 import java.sql.Date;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
 
 public class Semester {
-	private String name;
-	private Date startDate;
+    private final String name;
+    private final Date startDate;
 
-	public Semester(String name) {
-		this.name = name;
-		this.startDate = null;
-	}
+    public Semester(String name) {
+        this.name = name;
+        this.startDate = null;
+    }
 
-	public Semester(String name, Date startDate) {
-		this.name = name;
-		this.startDate = startDate;
-	}
-	
-	public String getName() {
-		return name;
-	}
-	
-	public String toString() {
-		return name;
-	}
+    public Semester(String name, Date startDate) {
+        this.name = name;
+        this.startDate = startDate;
+    }
 
-	public Date getStartDate() {
-		return startDate;
-	}
-	
-	
+    public String getName() {
+        return name;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }
