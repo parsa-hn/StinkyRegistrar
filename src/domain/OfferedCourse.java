eeb1939108393d7b3d92c482db7d0306d423ea78
parsa <hoseininejad1999@gmail.com>
2021-06-21 18:54:51 +0430

    Reformat OfferedCourse file

--- src/domain/OfferedCourse.java
+++ src/domain/OfferedCourse.java
@@ -1,43 +1,46 @@
 package domain;
+
 import java.util.Date;
 
 public class OfferedCourse {
-	private final Course course;
-	private final int section;
-	private final Date examDate;
+    private final Course course;
+    private final int section;
+    private final Date examDate;
 
-	public static int DEFAULT_SECTION = 1;
+    public static int DEFAULT_SECTION = 1;
 
-	public OfferedCourse(Course course) {
-		this.course = course;
-		this.section = DEFAULT_SECTION;
-		this.examDate = null;
-	}
+    public OfferedCourse(Course course) {
+        this.course = course;
+        this.section = DEFAULT_SECTION;
+        this.examDate = null;
+    }
 
-	public OfferedCourse(Course course, Date examDate) {
-		this.course = course;
-		this.section = DEFAULT_SECTION;
-		this.examDate = examDate;
-	}
+    public OfferedCourse(Course course, Date examDate) {
+        this.course = course;
+        this.section = DEFAULT_SECTION;
+        this.examDate = examDate;
+    }
 
-	public OfferedCourse(Course course, Date examDate, int section) {
-		this.course = course;
-		this.section = section;
-		this.examDate = examDate;
-	}
-	
-	public Course getCourse() {
-		return course;
-	}
-	
-	public Date getExamDate() {
-		return examDate;
-	}
+    public OfferedCourse(Course course, Date examDate, int section) {
+        this.course = course;
+        this.section = section;
+        this.examDate = examDate;
+    }
 
-	public int getSection() { return section; }
+    public Course getCourse() {
+        return course;
+    }
 
-	@Override
-	public String toString() {
-		return course.getName() + " - " + section;
-	}
+    public Date getExamDate() {
+        return examDate;
+    }
+
+    public int getSection() {
+        return section;
+    }
+
+    @Override
+    public String toString() {
+        return course.getName() + " - " + section;
+    }
 }
