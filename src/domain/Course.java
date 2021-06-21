71800bb8003f6b2b2fe92416ea63d96ce161c546
parsa <hoseininejad1999@gmail.com>
2021-06-21 18:33:43 +0430

    Make immutable fields in Course final

--- src/domain/Course.java
+++ src/domain/Course.java
@@ -4,16 +4,16 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Course {
-	private String id;
-	private String name;
-	private int units;
-	private List<Course> prerequisites;
+	private final String id;
+	private final String name;
+	private final int units;
+	private final List<Course> prerequisites;
 
 	public Course(String id, String name, int units) {
 		this.id = id;
 		this.name = name;
 		this.units = units;
-		prerequisites = new ArrayList<Course>();
+		prerequisites = new ArrayList<>();
 	}
 
 	public void addPrerequisites(Course... pres) {
